47662b3c2a94137454a2ff87524ae8554259abae
stshpaul1996 <113903052+stshpaul1996@users.noreply.github.com>
2022-10-18 16:48:49 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,23 @@
+# TASK-2
+  # Calculation SI and CI
+''' SI= Simple Interest
+    CI= Compound Interest
+    
+'''
+
+p=100           # prinicipl amt
+t=1             # tenure 
+r=13.5          # rate of interset
+a=p*t*r
+b=100
+si=(a/b)        # si=ptr/100
+print(si)
+
+
+p=15436         # prinicipl amt
+r=24            # rate of interset
+n=1             # no of years
+a=1+r
+b=a/100
+ci=p*b*n        # ci=p(1+r/100)n
+print(ci)
